2723a66fea4975dbe5f901b1668306a1e17e4691
Omer Perry <thebne@gmail.com>
2022-01-13 21:02:38 +0200

    fix some nasty velocity bugs

--- Scripts/Builtins/Modifiers/TransitionStrategies/VelocityStrategy.cs
+++ Scripts/Builtins/Modifiers/TransitionStrategies/VelocityStrategy.cs
@@ -6,15 +6,23 @@ namespace OneHamsa.Dexterity.Visual.Builtins
 {
     public class VelocityStrategy : BaseStrategy
     {
-        public float smoothTime = 1.5f;
+        public float smoothTime = 0.167f;
         public float maxSpeed = Mathf.Infinity;
 
-        private float currentVelocity = default;
+        private float[] currentVelocities; 
+
+        public override IDictionary<int, float> Initialize(int[] states, int currentState)  
+        {
+            currentVelocities = new float[states.Length];
+            return base.Initialize(states, currentState);
+        }
 
         protected override float GetStateValue(int state, int currentState, float currentValue, double deltaTime)
         {
             var targetValue = state == currentState ? 1 : 0;
-            return Mathf.SmoothDamp(currentValue, targetValue, ref currentVelocity, (float)(smoothTime * deltaTime), maxSpeed);
+            Debug.Log($"smoothtime = {(float)smoothTime} deltatime = {(float)deltaTime}");
+            return Mathf.SmoothDamp(currentValue, targetValue, ref currentVelocities[state], (float)smoothTime, maxSpeed,
+                (float)deltaTime);
         }
     }
 }
