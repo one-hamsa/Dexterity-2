f78a38074c4866896b20299a2cbaef207e3824f3
Davudh <dave@onehamsa.com>
2022-03-17 18:37:44 +0200

    nodes not node

--- Scripts/Builtins/Fields/NodeField.cs
+++ Scripts/Builtins/Fields/NodeField.cs
@@ -7,31 +7,75 @@ namespace OneHamsa.Dexterity.Visual.Builtins
 {
     public class NodeField : BaseField
     {
-        public Node targetNode;
+        public enum TakeValueWhen
+        {
+            AnyEqualsTrue = 0,
+            AnyEqualsFalse = 1,
+            AllEqual = 2,
+        }
+
+        public List<Node> targetNodes = new List<Node>();
         [Field]
         public string fieldName;
+        public TakeValueWhen takeValueWhen = TakeValueWhen.AnyEqualsTrue;
         public bool negate;
 
-        Node.OutputField outputField;
+        List<Node.OutputField> outputFields = new List<Node.OutputField>();
 
         // we always report about someone else's field, so mark as proxy
         public override bool proxy => true;
-        public override int GetValue() => negate ? (outputField.GetValue() + 1) % 2 : outputField.GetValue();
+        public override int GetValue() {
+            var value = GetValueBeforeNegation();
+            return negate ? (value + 1) % 2 : value;
+        }
+
+        private int GetValueBeforeNegation() {
+            switch (takeValueWhen) {
+                case TakeValueWhen.AnyEqualsTrue:
+                    foreach (var field in outputFields) {
+                        if (field.GetBooleanValue())
+                            return 1;
+                    }
+                    return 0;
+                case TakeValueWhen.AnyEqualsFalse:
+                    foreach (var field in outputFields) {
+                        if (field.GetBooleanValue())
+                            return 0;
+                    }
+                    return 1;
+                case TakeValueWhen.AllEqual:
+                    int? prevValue = null;
+                    foreach (var field in outputFields) {
+                        var value = field.GetValue();
+                        if (prevValue.HasValue && prevValue.Value != value)
+                            return 0;
+
+                        prevValue = value;
+                    }
+                    return prevValue.HasValue ? prevValue.Value : 0;
+            }
+            return 0;
+        }
 
         protected override void Initialize(Node context)
         {
             base.Initialize(context);
 
-            if (targetNode == null)
+            if (targetNodes.Count == 0)
             {
-                Debug.LogError($"target node == null", context);
+                Debug.LogError($"target nodes empty", context);
                 throw new FieldInitializationException();
             }
 
-            outputField = targetNode.GetOutputField(fieldName);
-            
             ClearUpstreamFields();
-            AddUpstreamField(outputField);
+            foreach (var node in targetNodes) {
+                if (node == null)
+                    continue;
+
+                Node.OutputField outputField = node.GetOutputField(fieldName);
+                outputFields.Add(outputField);
+                AddUpstreamField(outputField);
+            }
         }
     }
 }
