b7be1408cfc2cbfa71e337dee2d3824a2b1e557b
Boof <oferei@gmail.com>
2022-05-15 21:25:19 +0300

    renamed Orientation Modifier to Transform Modifier

--- Scripts/Builtins/Modifiers/Transform/TransformModifier.cs
+++ Scripts/Builtins/Modifiers/Transform/TransformModifier.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 
 namespace OneHamsa.Dexterity.Visual.Builtins
 {
-    public class OrientationModifier : Modifier, ISupportPropertyFreeze
+    public class TransformModifier : Modifier, ISupportPropertyFreeze
     {
         [Serializable]
         public class Property : PropertyBase
