c276ed7707b2fb63aa480f9612ee8266428280a2
boof <boof@onehamsa.com>
2022-07-04 17:44:42 +0300

    inserted SpecificStateListener into namespace

--- Scripts/Builtins/Listeners/SpecificStateListener.cs
+++ Scripts/Builtins/Listeners/SpecificStateListener.cs
@@ -2,56 +2,58 @@
 using UnityEngine;
 using UnityEngine.Events;
 
-public class SpecificStateListener : MonoBehaviour, IReferencesNode {
+namespace OneHamsa.Dexterity.Visual.Builtins {
+	public class SpecificStateListener : MonoBehaviour, IReferencesNode {
 
-	[Tooltip("Optional. Will look for any Node on object or it parents")]
-	[SerializeField]
-	protected Node node;
+		[Tooltip("Optional. Will look for any Node on object or it parents")]
+		[SerializeField]
+		protected Node node;
 
-	DexterityBaseNode IReferencesNode.node => GetComponentInParent<DexterityBaseNode>();
+		DexterityBaseNode IReferencesNode.node => GetComponentInParent<DexterityBaseNode>();
 
-	[State]
-	public string stateName;
+		[State]
+		public string stateName;
 
-	public UnityEvent OnEnterState;
-	public UnityEvent OnExitState;
+		public UnityEvent OnEnterState;
+		public UnityEvent OnExitState;
 
-	int _stateID;
+		int _stateID;
 
-	void Awake() {
-		if (!node)
-			node = GetComponentInParent<Node>();
+		void Awake() {
+			if (!node)
+				node = GetComponentInParent<Node>();
 
-		if (!node) {
-			Debug.LogError($"Node not found on ({gameObject.name})", this);
-			enabled = false;
-		}
-	}
-
-	void OnEnable() {
-		if (!node) return;
-		_stateID = Core.instance.GetStateID(stateName);
-		_inTheState = node.activeState == _stateID;
-		node.onStateChanged += OnStateChanged;
-	}
-
-	void OnDisable() {
-		if (!node) return;
-		node.onStateChanged -= OnStateChanged;
-	}
-
-	bool _inTheState = false;
-
-	bool inTheState {
-		set {
-			if (_inTheState != value) {
-				_inTheState = value;
-				(_inTheState ? OnEnterState : OnExitState)?.Invoke();
+			if (!node) {
+				Debug.LogError($"Node not found on ({gameObject.name})", this);
+				enabled = false;
 			}
 		}
-	}
 
-	void OnStateChanged(int oldValue, int newValue) {
-		inTheState = newValue == _stateID;
+		void OnEnable() {
+			if (!node) return;
+			_stateID = Core.instance.GetStateID(stateName);
+			_inTheState = node.activeState == _stateID;
+			node.onStateChanged += OnStateChanged;
+		}
+
+		void OnDisable() {
+			if (!node) return;
+			node.onStateChanged -= OnStateChanged;
+		}
+
+		bool _inTheState = false;
+
+		bool inTheState {
+			set {
+				if (_inTheState != value) {
+					_inTheState = value;
+					(_inTheState ? OnEnterState : OnExitState)?.Invoke();
+				}
+			}
+		}
+
+		void OnStateChanged(int oldValue, int newValue) {
+			inTheState = newValue == _stateID;
+		}
 	}
 }
