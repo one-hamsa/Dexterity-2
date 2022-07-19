4d7109923964206b084252269b785ee13d837565
boof <boof@onehamsa.com>
2022-07-19 15:06:11 +0300

    this too

--- Scripts/Builtins/Modifiers/GameObject/ActivateModifiersAwaker.cs
+++ Scripts/Builtins/Modifiers/GameObject/ActivateModifiersAwaker.cs
@@ -5,18 +5,18 @@
 // Problem solved: The ActivateModifier component may turn itself off during edit and saved.
 //                 It then never get activated during runtime.
 //
-// Solution: This component searches for turned-off SAMs and wakes them up.
+// Solution: This component searches for turned-off ActivateModifiers and wakes them up.
 
 namespace OneHamsa.Dexterity.Visual.Builtins {
 	public class ActivateModifiersAwaker : MonoBehaviour {
 
 		void Awake() {
-			foreach (var sam in GetComponentsInChildren<ActivateModifier>(includeInactive: true))
-				if (!sam.gameObject.activeSelf)
-					WakeUpSAM(sam);
+			foreach (var modifier in GetComponentsInChildren<ActivateModifier>(includeInactive: true))
+				if (!modifier.gameObject.activeSelf)
+					WakeUpActivateModifier(modifier);
 		}
 
-		void WakeUpSAM(ActivateModifier sam) {
+		void WakeUpActivateModifier(ActivateModifier sam) {
 			sam.gameObject.SetActive(true);
 		}
 	}
