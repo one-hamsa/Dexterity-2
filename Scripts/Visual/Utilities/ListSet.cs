6f1a7e5aea34974a17bbcf42a2c9dd7af0d8e664
Omer Perry <omer@onehamsa.com>
2022-08-15 14:49:26 +0300

    iconify

---
+++ Scripts/Visual/Utilities/ListSet.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+
+namespace OneHamsa.Dexterity.Visual.Utilities
+{
+	public class ListSet<T> : List<T> {
+
+
+		public ListSet() : base() { }
+
+		public ListSet(IEnumerable<T> collection) : base(collection) { }
+
+		public ListSet(int capacity) : base(capacity) { }
+
+		public new bool Add(T item)
+        {
+			if (Contains(item))
+				return false;
+
+			base.Add(item);
+			return true;
+        }
+
+		public new bool Remove(T item)
+        {
+			for (var i = 0; i < Count; ++i)
+				if (this[i].Equals(item))
+                {
+					RemoveAt(i);
+					return true;
+                }
+
+			return false;
+        }
+
+		public new void AddRange(IEnumerable<T> collection)
+        {
+			throw new NotImplementedException();
+        }
+	}
+}
