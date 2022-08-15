6f001966cd170a584b17d8968391b26005ec95c5
boof <boof@onehamsa.com>
2022-08-15 18:46:18 +0300

    added Linear style to SimpleStrategy (ITransitionStrategy)

--- Scripts/Builtins/Modifiers/TransitionStrategies/SimpleStrategy.cs
+++ Scripts/Builtins/Modifiers/TransitionStrategies/SimpleStrategy.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using UnityEngine;
 using System;
 
@@ -11,9 +10,13 @@ namespace OneHamsa.Dexterity.Visual.Builtins
         {
             ContinuousLerp,
             Discrete,
+            Linear
         }
 
+        [Tooltip("For Continuous Lerp")]
         public float transitionSpeed = 10f;
+        [Tooltip("For Linear")]
+        public float transitionTime = 1f;
         public TransitionStyle style = TransitionStyle.ContinuousLerp;
 
         protected override float GetStateValue(int state, int currentState, float currentValue, double deltaTime)
@@ -26,6 +29,12 @@ namespace OneHamsa.Dexterity.Visual.Builtins
                     return Mathf.Lerp(currentValue, targetValue, (float)(transitionSpeed * deltaTime));
                 case TransitionStyle.Discrete:
                     return targetValue;
+                case TransitionStyle.Linear:
+                    float changeSign = targetValue > currentValue ? 1 : -1;
+                    float timeLeft = (targetValue - currentValue) * changeSign * transitionTime;
+                    if (deltaTime >= timeLeft)
+                        return targetValue;
+                    return currentValue + (float)deltaTime * changeSign / transitionTime;
             }
             return default;
         }
