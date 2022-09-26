cee9173f467f04fe68a5692036e80c3741c19f97
Omer Perry <omer@onehamsa.com>
2022-09-26 17:23:24 +0300

    change activate modifier context to actually call awake

--- Scripts/Builtins/Modifiers/GameObject/ActivateModifierContext.cs
+++ Scripts/Builtins/Modifiers/GameObject/ActivateModifierContext.cs
@@ -11,9 +11,43 @@ namespace OneHamsa.Dexterity.Visual.Builtins
         private void OnEnable()
         {
             activateModifers = GetComponentsInChildren<ActivateModifier>(true);
-            
+
             foreach (var modifier in activateModifers)
-                modifier.Awake();
+            {
+                if (!modifier.enabled)
+                    continue;
+
+                if (!modifier.gameObject.activeInHierarchy)
+                {
+                    // just make sure the modifier finds its node before we reparent and enable it
+                    modifier._node = modifier.node;
+                    ForceAwake(modifier.gameObject);
+                }
+            }
+        }
+
+        private void ForceAwake(GameObject gameObject)
+        {
+            // save all transform data that's going to be changed
+            var parent = gameObject.transform.parent;
+            var activeSelf = gameObject.activeSelf;
+            var localPosition = gameObject.transform.localPosition;
+            var localRotation = gameObject.transform.localRotation;
+            var localScale = gameObject.transform.localScale;
+            var siblingIndex = gameObject.transform.GetSiblingIndex();
+            
+            // set parent to root
+            gameObject.transform.SetParent(transform);
+            // make sure it's enabled
+            gameObject.SetActive(true);
+            
+            // set transform data back to what it was
+            gameObject.SetActive(activeSelf);
+            gameObject.transform.SetParent(parent);
+            gameObject.transform.SetSiblingIndex(siblingIndex);
+            gameObject.transform.localPosition = localPosition;
+            gameObject.transform.localRotation = localRotation;
+            gameObject.transform.localScale = localScale;
         }
     }
 }
