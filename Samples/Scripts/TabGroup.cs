e59c3f1b758d74427b719fd0eb42bbdcd8bf40a0
Omer Perry <thebne@gmail.com>
2023-06-18 00:15:24 +0300

    more examples

---
+++ Samples/Scripts/TabGroup.cs
@@ -0,0 +1,32 @@
+using OneHamsa.Dexterity;
+using OneHamsa.Dexterity.Builtins;
+using UnityEngine;
+
+public class TabGroup : MonoBehaviour
+{
+    public int selectedTabIndex;
+    [Field] public string focusFieldName;
+
+    private void OnEnable()
+    {
+        SelectTab(selectedTabIndex);
+        
+        for (var i = 0; i < transform.childCount; i++)
+        {
+            var current = i;
+            transform.GetChild(i).transform.GetComponent<ClickListener>()
+                .onClick.AddListener(() => SelectTab(current));
+        }
+    }
+
+    public void SelectTab(int index)
+    {
+        selectedTabIndex = index;
+        for (var i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).GetComponent<Node>()
+                .GetOutputField(focusFieldName)
+                .SetOverride(i == index);
+        }
+    }
+}
