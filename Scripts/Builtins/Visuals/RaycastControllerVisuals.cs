3701add3efcfd16936f14b9bf4c1122a6be49bc7
Omer Perry <omer@onehamsa.com>
2023-06-22 20:00:25 +0300

    pointer dest ray support

--- Scripts/Builtins/Visuals/RaycastControllerVisuals.cs
+++ Scripts/Builtins/Visuals/RaycastControllerVisuals.cs
@@ -8,7 +8,9 @@ namespace OneHamsa.Dexterity.Visual.Builtins
     public class RaycastControllerVisuals : MonoBehaviour
     {
         [SerializeField] LineRenderer lineRenderer;
+        [SerializeField] LineRenderer destinationLineRenderer;
         [SerializeField] float maxLength = 10;
+        [SerializeField] float maxDestLength = 1;
 
         RaycastController controller;
 
@@ -19,6 +21,15 @@ namespace OneHamsa.Dexterity.Visual.Builtins
         // Update is called once per frame
         void Update()
         {
+            UpdateSourceLineRenderer();
+            UpdateDestinationLineRenderer();
+        }
+
+        private void UpdateSourceLineRenderer()
+        {
+            if (lineRenderer == null)
+                return;
+            
             lineRenderer.enabled = controller.current;
             
             Vector3 origin = controller.ray.origin;
@@ -33,6 +44,22 @@ namespace OneHamsa.Dexterity.Visual.Builtins
             {
                 lineRenderer.SetPosition(1, controller.ray.origin + controller.ray.direction * maxLength);
             }
+        }
+
+        private void UpdateDestinationLineRenderer()
+        {
+            if (destinationLineRenderer == null)
+                return;
+            
+            destinationLineRenderer.enabled = controller.current && controller.didHit;
+
+            var destToOrigin = controller.ray.origin - controller.hit.point;
+            if (destToOrigin.sqrMagnitude > maxDestLength * maxDestLength)
+                destToOrigin = destToOrigin.normalized * maxDestLength;
+            
+            destinationLineRenderer.SetPosition(0, controller.hit.point);
+            destinationLineRenderer.SetPosition(1, controller.hit.point + destToOrigin * .01f); 
+            destinationLineRenderer.SetPosition(2, controller.hit.point + destToOrigin); 
         }
     }
 }
