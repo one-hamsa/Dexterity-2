b5e808d56027de703ce47b38280af41cfcbd7e4a
Omer Perry <thebne@gmail.com>
2023-07-23 00:18:32 +0300

    binding field

---
+++ Scripts/Builtins/Fields/BindingField.cs
@@ -0,0 +1,34 @@
+using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.Events;
+
+namespace OneHamsa.Dexterity.Builtins
+{
+    public class BindingField : BaseField
+    {
+        public BoolObjectBinding binding;
+        public bool negate;
+
+        protected override void Initialize(FieldNode context)
+        {
+            base.Initialize(context);
+
+            if (!binding.IsValid())
+                return;
+            
+            if (!binding.Initialize())
+                Debug.LogError($"{nameof(BindingField)}: Failed to initialize binding for {context}", context);
+        }
+
+        public override int GetValue() 
+        {
+            if (!binding.IsValid() || !binding.IsInitialized())
+                return negate ? 1 : 0;
+
+            var value = binding.Boolean_GetValue() ? 1 : 0;
+            return negate ? (value + 1) % 2 : value;
+        }
+    }
+}
