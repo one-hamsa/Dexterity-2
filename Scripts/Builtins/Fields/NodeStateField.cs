86985bc44f3bfe0842ee7552d1a6d1d1580388af
Omer Perry <thebne@gmail.com>
2024-05-08 16:49:35 +0300

    fix broken node state field

--- Scripts/Builtins/Fields/NodeStateField.cs
+++ Scripts/Builtins/Fields/NodeStateField.cs
@@ -24,12 +24,34 @@ namespace OneHamsa.Dexterity.Builtins
             
             targetStateId = Database.instance.GetStateID(targetState);
             targetNode.onStateChanged += OnTargetNodeStateChanged;
+            targetNode.onEnabled += TestNodeState;
+            targetNode.onDisabled += TestNodeState;
+            
+            if (targetNode.initialized)
+                OnTargetNodeStateChanged(0, targetNode.GetActiveState());
+        }
+
+        public override void Finalize(FieldNode context)
+        {
+            base.Finalize(context);
+            
+            if (targetNode != null)
+            {
+                targetNode.onStateChanged -= OnTargetNodeStateChanged;
+                targetNode.onEnabled -= TestNodeState;
+                targetNode.onDisabled -= TestNodeState;
+            }
+        }
+
+        private void TestNodeState()
+        {
+            var v = targetNode.GetActiveState() == targetStateId ? 1 : 0;
+            SetValue(negate ? (v + 1) % 2 : v);
         }
 
         private void OnTargetNodeStateChanged(int oldState, int newState)
         {
-            var v = targetNode.GetActiveState() == targetStateId ? 1 : 0;
-            SetValue(negate ? (v + 1) % 2 : v);
+            TestNodeState();
         }
     }
 }
